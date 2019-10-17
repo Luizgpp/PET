a5e12771371ab59f38123e73218bda95e414ad36
luizgpp <luizgpp@gmail.com>
2019-10-16 22:35:43 -0300

    Criando menu

---
+++ menu.php
@@ -0,0 +1,172 @@
+<ul id="dropdown1" class="dropdown-content">
+    <li><a href="cadfuncionario.php">Cadastrar Funcionário</a></li>
+    <li class="divider"></li>
+    <li><a href="listfuncionario.php">Gerenciar Funcionários</a></li>
+</ul>
+<ul id="dropdown2" class="dropdown-content">
+    <li><a href="cadcliente.php">Cadastrar Cliente</a></li>
+    <li class="divider"></li>
+    <li><a href="listcliente.php">Gerenciar Clientes</a></li>
+</ul>
+<ul id="dropdown3" class="dropdown-content">
+    <li><a href="cadfornecedor.php">Cadastrar Fornecedor</a></li>
+    <li class="divider"></li>
+    <li><a href="listfornecedor.php">Gerenciar Fornecedores</a></li>
+</ul>
+<ul id="dropdown4" class="dropdown-content">
+    <li><a href="cadproduto.php">Cadastrar Produtos</a></li>
+    <li class="divider"></li>
+    <li><a href="listproduto.php">Gerenciar Produtos</a></li>
+</ul>
+<ul id="dropdown5" class="dropdown-content">
+    <li><a href="venda.php">Realizar Venda</a></li><br>
+    <li class="divider"></li>
+    <li><a href="listvenda.php">Gerenciar Vendas</a></li><br>
+    <li class="divider"></li>
+    <li><a href="agendamento.php">Agendamentos</a></li><br>
+</ul>
+<ul id="dropdown6" class="dropdown-content">
+    <li><a href=#>Relatório de Funcionários</a></li><br>
+    <li><a href=#>Relatório de Clientes</a></li><br>
+    <li><a href=#>Relatório de Fornecedores</a></li><br>
+    <li><a href=#>Relatório de Produtos</a></li><br>
+    <li><a href=#>Relatório de Vendas</a></li><br>
+    <li><a href=#>Relatório Financeiro</a></li><br>
+</ul>
+<ul id="dropdown7" class="dropdown-content">
+    <li><a href="alterarsenha.php">Alterar Senha</a></li>
+    <li class="divider"></li>
+    <li><a href="logout.php">Sair</a></li>
+</ul>
+
+<nav>
+    <div class="nav-wrapper teal darken-4">
+        <a href="#!" class="brand-logo">Logo</a>
+        <a href="#" data-target="mobile-demo" class="sidenav-trigger fixed"><i class="material-icons">menu</i></a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="home.php">Home</a></li>
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown1">Funcionarios<i class="material-icons right">arrow_drop_down</i></a></li>
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown2">Clientes<i class="material-icons right">arrow_drop_down</i></a></li>
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown3">Fornecedores<i class="material-icons right">arrow_drop_down</i></a></li>
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown4">Produtos<i class="material-icons right">arrow_drop_down</i></a></li>
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown5">Vendas/Serviços<i class="material-icons right">arrow_drop_down</i></a></li>
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown6">Relatórios<i class="material-icons right">arrow_drop_down</i></a></li>
+            <li><a class="dropdown-trigger" href="#!" data-target="dropdown7">Conta<i class="material-icons right">arrow_drop_down</i></a></li>
+        </ul>
+    </div>
+</nav>
+
+<ul class="sidenav" id="mobile-demo">
+    <li>
+        <div class="user-view">
+            <div class="background">
+                <img src="http://placehold.it/640/555">
+            </div>
+            <a href="#!"><img class="circle" src="http://placehold.it/640/333"></a>
+            <a href="#!"><span class="white-text name">John Doe</span></a>
+            <a href="#!"><span class="white-text email">jdandturk@gmail.com</span></a>
+        </div>
+    </li>
+    <li><a class="waves-effect" href="home.php">Home</a></li>
+    <li class="divider"></li>
+
+    <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+            <li>
+                <a class="collapsible-header waves-effect">Funcionarios<i class="material-icons">folder</i></a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="cadfuncionario.php">Cadastrar Funcionário</a></li>
+                        <li class="divider"></li>
+                        <li><a href="listfuncionario.php">Gerenciar Funcionários</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>
+    <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+            <li>
+                <a class="collapsible-header waves-effect">Clientes<i class="material-icons">folder</i></a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="cadcliente.php">Cadastrar Cliente</a></li>
+                        <li class="divider"></li>
+                        <li><a href="listcliente.php">Gerenciar Clientes</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>
+    <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+            <li>
+                <a class="collapsible-header waves-effect">Fornecedores<i class="material-icons">folder</i></a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="cadfuncionario.php">Cadastrar Funcionário</a></li>
+                        <li class="divider"></li>
+                        <li><a href="listfuncionario.php">Gerenciar Funcionários</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>
+    <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+            <li>
+                <a class="collapsible-header waves-effect">Produtos<i class="material-icons">folder</i></a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="cadfuncionario.php">Cadastrar Funcionário</a></li>
+                        <li class="divider"></li>
+                        <li><a href="listfuncionario.php">Gerenciar Funcionários</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>
+    <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+            <li>
+                <a class="collapsible-header waves-effect">Vendas/Serviços<i class="material-icons">folder</i></a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="cadfuncionario.php">Cadastrar Funcionário</a></li>
+                        <li class="divider"></li>
+                        <li><a href="listfuncionario.php">Gerenciar Funcionários</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>
+    <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+            <li>
+                <a class="collapsible-header waves-effect">Relatórios<i class="material-icons">folder</i></a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="cadfuncionario.php">Cadastrar Funcionário</a></li>
+                        <li class="divider"></li>
+                        <li><a href="listfuncionario.php">Gerenciar Funcionários</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>
+    <li class="divider"></li>
+    <li class="no-padding">
+        <ul class="collapsible collapsible-accordion">
+            <li>
+                <a class="collapsible-header waves-effect">Conta<i class="material-icons">folder</i></a>
+                <div class="collapsible-body">
+                    <ul>
+                        <li><a href="cadfuncionario.php">Cadastrar Funcionário</a></li>
+                        <li class="divider"></li>
+                        <li><a href="listfuncionario.php">Gerenciar Funcionários</a></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+    </li>
+</ul>
