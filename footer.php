9bf843eebe239398324eb9abfe2f65de86ff6391
Luizgpp <luizgpp@gmail.com>
2019-10-17 14:08:31 -0300

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
-    <!--JavaScript at end of body for optimized loading-->
+<!--JavaScript at end of body for optimized loading-->
 
-    <script type="text/javascript" src="js/materialize.min.js"></script>
-    <script type="text/javascript" src="js/main.js"></script>
-    </body>
+<script type="text/javascript" src="js/materialize.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</body>
 
-    </html>
+</html>
